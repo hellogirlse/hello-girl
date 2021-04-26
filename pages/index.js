6cde5d073c7772a10edfee33cd4bf314ce817e35
Linnea Karlsson <linnea.karlsson@consid.se>
2021-04-26 10:32:18 +0200

    Text and links on landingpage

--- pages/index.js
+++ pages/index.js
@@ -36,12 +36,66 @@ export default function Home() {
           className={styles.logo}
           src="/logo/logo.svg"
         ></img>
+        <p>
+          <strong>Hello Girl! </strong>
+          is a coding network for girls and women. The purpose of the network is
+          to inspire, share knowledge and exchange experience about how it’s
+          like to work with tech. Eventually this will increase gender equality
+          in the technical field. Hello Girl! started in Jönköping, in October
+          2018, as a two day long web programming course arranged by Science
+          Park, local organisations and companies and has now grown to so much
+          more than that. You can find Hello Girl! on Clubhouse, LinkedIn,
+          GitHub, Facebook and Instagram.
+        </p>
+        <p>
+          <em>
+            “We need more women in the technical field so that they also are a
+            part of developing the technology that everyone will use. We need
+            diversity also in this profession.”
+          </em>
+          <br />
+          Linnea Karlsson, teacher Hello Girl!
+        </p>
       </main>
 
       <footer className={styles.footer}>
-        <a href="https://www.linkyar next devdin.com/groups/12518492/">
-          Följd oss på LinkedIn för senaste nyheterna!
-        </a>
+        <div className={styles.socialMedia}>
+          <a
+            href="https://ios.joinclubhouse.com/club/hello-girl"
+            title="Clubhouse"
+            target="_blank"
+          >
+            Clubhouse
+          </a>
+          <a
+            href="https://www.linkedin.com/groups/12518492/"
+            title="LinkedIn"
+            target="_blank"
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+          <a
+            href="https://github.com/hellogirlse"
+            title="GitHub"
+            target="_blank"
+          >
+            <i className="fab fa-github"></i>
+          </a>
+          <a
+            href="https://www.facebook.com/groups/575179392961804"
+            title="Facebook"
+            target="_blank"
+          >
+            <i className="fab fa-facebook-f"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/hellogirlse/"
+            title="Instagram"
+            target="_blank"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+        </div>
       </footer>
     </div>
   );
