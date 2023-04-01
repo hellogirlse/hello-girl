0a2508b28e8c02a4eedf2529f1dad6d62f943900
daniela-waara <daniela.waara@avantime.se>
2023-04-01 14:06:24 +0200

    FEAT: Remove event and update text

--- components/primarySection/textPrimary.tsx
+++ components/primarySection/textPrimary.tsx
@@ -1,32 +1,39 @@
-import style from './textPrimary.module.scss';
+import style from "./textPrimary.module.scss";
 
 function PrimarySection(props) {
-    return (
-        <section id="om-oss" className={`bg-primary ${style.primary}`}>
-            <div className={`container ${style.primary__container}`}>
+  return (
+    <section id='om-oss' className={`bg-primary ${style.primary}`}>
+      <div className={`container ${style.primary__container}`}>
+        <div className={`page-decoration ${style.primary__decoration}`}>
+          <fieldset>
+            <legend aria-hidden='true'>Vår drivkraft</legend>
+          </fieldset>
+        </div>
 
-                <div className={`page-decoration ${style.primary__decoration}`}>
-                    <fieldset>
-                        <legend aria-hidden="true">Vår drivkraft</legend>
-                    </fieldset>
-                </div>
+        <div className={style.primary__container__wrapper}>
+          <h2 className='headline headline--h2'>
+            Vi brinner för programmering
+          </h2>
+          <p>
+            Idag är det ca. 30% som arbetar inom IT-sektorn i Sverige som är
+            kvinnor. Detta vill Hello Girl! ändra på. Syftet med Hello Girl! är
+            att genom workshops sprida kunskap och öka intresset för
+            programmering och tech bland tjejer och kvinnor. (Statistik tagen
+            från IT- och Telekomföretagen)
+          </p>
 
-                <div className={style.primary__container__wrapper}>
-                    <h2 className="headline headline--h2">Vi brinner för programmering</h2>
-                    <p>
-                        Idag är det endast 28% som arbetar inom IT-sektorn i Sverige som är kvinnor. 
-                        Detta vill Hello Girl! ändra på. 
-                        Syftet med Hello Girl! är att genom workshops sprida kunskap och öka intresset för programmering och tech bland tjejer och kvinnor. (Statistik tagen från IT- och Telekomföretagen)
-                    </p>
-
-                    <p>
-                        Sedan starten 2018 har våra workshops verkat i flera omgångar, och totalt har över 80 kvinnor (!) testat på programmering och inspirerats genom Hello Girl! Deltagarna har varit tjejer och kvinnor i olika åldrar, nationaliteter och med bakgrunder. 
-                        Vissa med funderingar om att söka en IT-utbildning, några med viljan om att göra ett karriärbyte och andra som vill få mer förståelse av tech för att öka sina chanser till jobb.
-                    </p>
-                </div>
-
-            </div>
-        </section>
-    );
+          <p>
+            Sedan starten 2018 har våra workshops verkat i flera omgångar, och
+            totalt har över 80 kvinnor (!) testat på programmering och
+            inspirerats genom Hello Girl! Deltagarna har varit tjejer och
+            kvinnor i olika åldrar, nationaliteter och med bakgrunder. Vissa med
+            funderingar om att söka en IT-utbildning, några med viljan om att
+            göra ett karriärbyte och andra som vill få mer förståelse av tech
+            för att öka sina chanser till jobb.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 export default PrimarySection;
