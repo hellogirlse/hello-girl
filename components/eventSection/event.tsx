3645d54a726fd5439636739d08aec76267db4a29
daniela-waara <daniela.waara@avantime.se>
2024-06-25 11:04:23 +0200

    fix: update eventcards

--- components/eventSection/event.tsx
+++ components/eventSection/event.tsx
@@ -1,38 +1,47 @@
-import style from "./event.module.scss";
-import EventCard from "../eventCard/eventCard";
-import Button from "../button/button";
+import style from './event.module.scss';
+import EventCard from '../eventCard/eventCard';
+import Button from '../button/button';
 
 function EventSection(props) {
-  return (
-    <section id='event-och-kurser' className={style.event}>
-      <div className={`container ${style.event__container}`}>
-        <div className='page-decoration'>
-          <fieldset>
-            <legend aria-hidden='true'>Kurser & Event</legend>
-          </fieldset>
-        </div>
+    return (
+        <section id="event-och-kurser" className={style.event}>
+            <div className={`container ${style.event__container}`}>
+                <div className="page-decoration">
+                    <fieldset>
+                        <legend aria-hidden="true">Kurser & Event</legend>
+                    </fieldset>
+                </div>
 
-        <div className={style.event__container__wrapper}>
-          <div className={style.event__content}>
-            <h2 className='headline headline--h2'>Kurser & Event</h2>
-            {/* <p>Vi har för tillfället inga planerade event, håll utkik i våra sociala medier för kommande kurser & event.</p> */}
-            <p>
-              Är du nyfiken på att lära dig programmering?
-              <br />
-              Då är det här en utbildning för dig! Under våra workshops går vi
-              igenom grunderna i HTML, CSS och JavaScript för att bygga en enkel
-              och snygg webbsida.
-            </p>
-          </div>
+                <div className={style.event__container__wrapper}>
+                    <div className={style.event__content}>
+                        <h2 className="headline headline--h2">
+                            Kurser & Event
+                        </h2>
+                        {/* <p>Vi har för tillfället inga planerade event, håll utkik i våra sociala medier för kommande kurser & event.</p> */}
+                        <p>
+                            Är du nyfiken på att lära dig programmering?
+                            <br />
+                            Då är det här en utbildning för dig! Under våra
+                            workshops går vi igenom grunderna i HTML, CSS och
+                            JavaScript för att bygga en enkel och snygg
+                            webbsida.
+                        </p>
+                    </div>
 
-          <EventCard
-            title='Kurs i Jönköping'
-            date='5-6 april'
-            link={"https://hellogirl2024.confetti.events/"}
-          />
-        </div>
-      </div>
-    </section>
-  );
+                    <EventCard
+                        title="Inspirations-event i Jönköping"
+                        date="Hösten 2024"
+                        link={null}
+                    />
+
+                    <EventCard
+                        title="Kurs i Jönköping"
+                        date="Våren 2025"
+                        link={null}
+                    />
+                </div>
+            </div>
+        </section>
+    );
 }
 export default EventSection;
